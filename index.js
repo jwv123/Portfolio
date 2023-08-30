33b52f40cde15bef7b75091a2a6f24df3a25fa1f
jwv123 <james.william.viviers@gmail.com>
2023-08-30 21:23:15 +0200

    Added headers

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = require("express")();
 require("dotenv").config();
 
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send("Page Under Construction");
 });
